1d9363414c64f24b48e02a27da494ce54cff4431
yntik <eifaurya@gmail.com>
2019-01-11 16:39:26 +0200

    fix position of  button logout

--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -8,6 +8,7 @@ import {getMasters} from "../../store/action/mastersAction";
 class HeaderComponent extends React.Component {
 
     ROUTES = ['/homepage', '/masters', '/cities', '/orders', '/clients', '/products', '/callback'];
+
     constructor(props) {
         super(props);
         this.logout = this.logout.bind(this);
@@ -33,38 +34,36 @@ class HeaderComponent extends React.Component {
         }
         return <header>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div className="alert alert-dismissible alert-secondary">
+                    Clockwise Clockware.
+                </div>
                 <div className="collapse navbar-collapse" id="navbarColor01">
-                    <div className="alert alert-dismissible alert-secondary">
-                        Clockwise Clockware.
-                    </div>
-                    <div>
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <a className="btn btn-info" href='/homepage'>Главная страница</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="btn btn-info" href='/masters'>Список мастеров</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="btn btn-info" href='/cities'>Список городов</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="btn btn-info" href='/orders'>Список заказов</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="btn btn-info" href='/clients'>Список клиентов</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="btn btn-info" href='/products'>Ценовая политика</a>
-                            </li>
-                            <li className="nav-item">
-                                <button style={{position: 'absolute', right: 20, top: 20}} type="submit"
-                                        onClick={this.logout}
-                                        className="btn btn-danger">Выход
-                                </button>
-                            </li>
-                        </ul>
-                    </div>
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <a className="btn btn-info" href='/homepage'>Главная страница</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="btn btn-info" href='/masters'>Список мастеров</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="btn btn-info" href='/cities'>Список городов</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="btn btn-info" href='/orders'>Список заказов</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="btn btn-info" href='/clients'>Список клиентов</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="btn btn-info" href='/products'>Ценовая политика</a>
+                        </li>
+                    </ul>
+                    <form className="form-inline my-2 my-lg-0">
+                        <button type="submit"
+                                onClick={this.logout}
+                                className="btn btn-danger">Выход
+                        </button>
+                    </form>
                 </div>
             </nav>
         </header>
