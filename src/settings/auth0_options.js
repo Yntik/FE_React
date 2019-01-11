c6b17d8aab5803b5eab1e9b20dca3b86bb7a9095
yntik <eifaurya@gmail.com>
2019-01-11 16:58:42 +0200

    auth0 option

--- src/settings/auth0_options.js
+++ src/settings/auth0_options.js
@@ -2,10 +2,10 @@ export const auth0_option = {
     WebAuth: {
         domain: 'clockware.eu.auth0.com',
         clientID: 'f22JAIJHeWSAVgabhgTPeCOlkNzxsVCq',
-        redirectUri: 'https://shrouded-spire-22347.herokuapp.com/callback',
+        redirectUri: 'https://mighty-harbor-39325.herokuapp.com/callback',
         responseType: 'token id_token',
         scope: 'openid',
         audience: 'https://bc33edb6.ngrok.io/'
     },
-    returnTo: 'https://shrouded-spire-22347.herokuapp.com/login',
+    returnTo: 'https://mighty-harbor-39325.herokuapp.com/login',
 }
